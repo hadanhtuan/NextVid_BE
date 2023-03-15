95372579ec63865caeb3b7c7d656495986078d73
hadanhtuan <20522109@gm.uit.edu.vn>
2023-03-16 00:28:02 +0700

    shiet

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -28,7 +28,8 @@ export class AuthService {
     if (!firebase.apps.length) {
       firebase.initializeApp({
         credential: firebase.credential.cert({
-          privateKey: process.env.FIREBASE_PRIVATE_KEY,
+          privateKey: process.env.FIREBASE_PRIVATE_KEY ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/gm, "\n")
+          : undefined,
           clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
           projectId: process.env.FIREBASE_PROJECT_ID,
         }),
